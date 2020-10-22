3ad8ee611799ce79e62de031eb2e0eb983ca3c06
leetsyplakov <36146133+leetsyplakov@users.noreply.github.com>
2020-10-22 15:26:17 -0700

    added comments

--- yt_downloader.py
+++ yt_downloader.py
@@ -5,28 +5,26 @@ import csv
 # pip3 install --upgrade ffmpeg
 
 
-def download(video_id):
-    base_url = "https://www.youtube.com/watch?v="
-    dfolder_path = ""
-    dl_url = base_url + video_id
-    print(dl_url)
-    os.system(
-        f"youtube-dl https://www.youtube.com/watch?v={video_id} --format 'bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best' --merge-output-format mp4")
-
-
 def main():
+    #TRY-CATCH Block, to catch errors
     try:
-        csv_data = None
-        with open("lists/new_list.csv") as fp:
+        csv_data = None                                             # List to store the video ids
+        with open("lists/new_list.csv") as fp:                      # Open the CSV file
+            reader = csv.reader(fp, delimiter=",", quotechar='"')   # Set up the reader
+            next(reader, None)                                      # Skip the headers
+            csv_data = [row[0] for row in reader]                   # Read each CSV row to the list
 
-            reader = csv.reader(fp, delimiter=",", quotechar='"')
-            next(reader, None)  # skip the headers
-            csv_data = [row[0] for row in reader]
-
-        for i in csv_data:
-            video_id = i
-            print(video_id)
-            download(i)
+        for video_id in csv_data:                                   # Iterrate through the list
+            print(f"Downloading {video_id}")                        # Print out the video id from the list
+            '''
+            Calling YouTube-dl using os.system
+                youtube-dl
+                https://www.youtube.com/watch?v=[PASSIGN VIDEO ID]
+                --format 'bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best'
+                --merge-output-format mp4
+            '''
+            os.system(
+                f"youtube-dl https://www.youtube.com/watch?v={video_id} --format 'bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best' --merge-output-format mp4")
 
     except Exception as e:
         print(e)
